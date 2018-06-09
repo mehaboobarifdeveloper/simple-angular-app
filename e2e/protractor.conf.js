7afec183b60dc19b2bf71fec36674639099d04e3
mehaboobarif <mehaboobarif@outlook.com>
2018-06-09 13:55:07 +0530

    config

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -30,7 +30,7 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     // jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-    // jasmine.getEnv().addReporter(
+    jasmine.getEnv().addReporter(
     //   // new Jasmine2HtmlReporter({
     //   //   savePath: 'target/screenshots',
     //   //   takeScreenshots: false,
