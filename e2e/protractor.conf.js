80b5f737aa0f87e219cbe8f0f651ce48faae1ecc
mehaboobarif <mehaboobarif@outlook.com>
2018-06-10 00:26:28 +0530

    new config

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -4,7 +4,7 @@ exports.config = {
     capabilities: {
         'browserName': 'chrome',
         'chromeOptions':{
-            'args': ['--headless','--disable-gpu']
+            'args': ['--headless','--disable-gpu','--no-sandbox']
         }
       },
     allScriptsTimeout: 60000, 
