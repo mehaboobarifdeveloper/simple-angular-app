ae960d537f06d8e7879e73e6875a3b39222eb9e9
mehaboobarifdeveloper <mehaboobarifdeveloper@gmail.com>
2018-06-12 09:57:01 +0530

    Junit

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -14,5 +14,26 @@ exports.config = {
         defaultTimeoutInterval: 30000,
       },
     baseUrl: 'http://localhost:4200/',
+  
+  onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    var junitReporter = new jasmineReporters.JUnitXmlReporter({
+
+      // setup the output path for the junit reports
+      savePath: 'output/',
+
+      // conslidate all true:
+      //   output/junitresults.xml
+      //
+      // conslidate all set to false:
+      //   output/junitresults-example1.xml
+      //   output/junitresults-example2.xml
+      consolidateAll: false
+
+    });
+    jasmine.getEnv().addReporter(junitReporter);
+  }
+  
+
 
 }
